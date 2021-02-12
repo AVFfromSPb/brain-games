6fb66449147c1525b4fb4ae547dd03ca14752650
Aleksandr Fedorov <a.v.fedorov5@gmail.com>
2021-02-12 10:13:59 +0300

    Add new game "Brain-progression"

--- src/Engine.php
+++ src/Engine.php
@@ -7,13 +7,15 @@ use function Cli\prompt;
 use function BrainGames\src\Games\Even\getTaskEven;
 use function BrainGames\src\Games\Calc\getTaskCalc;
 use function BrainGames\src\Games\Gcd\getTaskGcd;
+use function BrainGames\src\Games\Progression\getTaskProgression;
 
 function startBrainGames($gamesType)
 {
     $tasks = [
         'even' => 'Answer "yes" if the number is even, otherwise answer "no".',
         'calc' => 'What is the result of the expression?',
-        'gcd' => 'Find the greatest common divisor of given numbers.'
+        'gcd' => 'Find the greatest common divisor of given numbers.',
+        'progression' => 'What number is missing in the progression?'
     ];
     line('Welcome to the Brain Games!');
     $task = $tasks[$gamesType];
@@ -35,6 +37,9 @@ function startBrainGames($gamesType)
                 break;
             case 'gcd':
                 [$expression, $correctAnswer] = getTaskGcd();
+                break;
+            case 'progression':
+                [$expression, $correctAnswer] = getTaskProgression();
                 break;
         }
         line();
---
+++ src/Games/Progression.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BrainGames\src\Games\Progression;
+
+function getTaskProgression(): array
+{
+    $numberLine = 10;
+    $firstNumber = rand(1, 10);
+    $numberOfHidden = rand(0, 10);
+    $difference = rand(1, 5);
+
+    $progression = [];
+
+    for ($i = 0; $i < $numberLine; $i++) {
+        if ($i !== $numberOfHidden) {
+            $progression[] = $firstNumber + ($difference * $i);
+        } else {
+            $progression[] = '..';
+            $correctAnswer = (string) ($firstNumber + ($difference * $i));
+        }
+    }
+    $expression = implode(' ', $progression);
+    return [$expression, $correctAnswer];
+}
